525de03c8993618565d01be606bfe2bef3afca41
ash0411 <aashishbora36@gmail.com>
2019-12-28 17:25:39 +0530

    socket and aws s3 lecture 8

---
+++ python_jupyter/testing/sender.py
@@ -0,0 +1,19 @@
+import socket
+target_ip = "192.168.43.63"
+target_port = 1233
+# now we are creating udp socket -- this is for all sender and reciever
+# it means we use ipv4,UDP
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+#this is only for reciever
+# s.bind((target_ip,target_port))
+
+
+#this is for sender
+msg = input("please enter your message : ")
+#now we can send to target
+new_msg = msg.encode('ascii')
+#print(new_msg)
+# we have to encode string to byte like object in python3 only
+s.sendto(new_msg,(target_ip,target_port))
+print(s.recvfrom(100))
+ 
