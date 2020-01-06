56a6f2d5ccce46d5ae4ce49256b225fa7b26786f
ash0411 <aashishbora36@gmail.com>
2020-01-06 11:08:35 +0530

    sender reciever completed for both sides

--- assignment/reciever.py
+++ assignment/reciever.py
@@ -1,5 +1,5 @@
 target_ip = "127.0.0.1"
-target_port = 1235
+target_port = 1236
 from cryptography.fernet import Fernet
 # now we are creating udp socket -- this is for all sender and reciever
 # it means we use ipv4,UDP
@@ -11,20 +11,24 @@ while True:
    # time.sleep(1)
 	client_data = s.recvfrom(1000)
 	#print(client_data)
-	client_datas = client_data[0].decode('ascii')
-	client_datab = client_datas.encode() 
+	#client_datas = client_data[0].decode('ascii')
+	#client_datab = client_datas.encode() 
 	#print(type(client_datab))
 	cipher_key =b'GzDqzcu4kz52ZOyu7haSaWY4t4mE2jzSKD4JuYfm7VE='
 	cipher = Fernet(cipher_key)
-	decrypted_message = cipher.decrypt(client_datab)   #decrypted_message = cipher.decrypt(encrypted_message)
-	print("\nreceived message =",str(decrypted_message.decode("ascii")))
+	decrypted_message = cipher.decrypt(client_data[0])   #decrypted_message = cipher.decrypt(encrypted_message)
+	decrypted_message_s = decrypted_message.decode("ascii")
+	print("\nreceived message =",decrypted_message_s)
 	audio1 = pyttsx3.init()
-	audio1.say(decrypted_message)
+	audio1.say(decrypted_message_s)
 	audio1.runAndWait()
 	print(client_data)
 	print("now replying  to ",client_data[1][0])
 	reply = input("enter the reply")
-	s.sendto(reply.encode('ascii'),client_data[1])
+	reply_b = reply.encode()
+	encrypted_message = cipher.encrypt(reply_b)
+	encrypted_message_s = encrypted_message.decode()
+	s.sendto(encrypted_message_s.encode('ascii'),client_data[1])
 	subprocess.getoutput("touch " + client_data[1][0] + ".txt")
 	with open(client_data[1][0]+".txt","a") as f:
 		f.write(client_data[0].decode('ascii'))
--- assignment/sender.py
+++ assignment/sender.py
@@ -1,7 +1,7 @@
-import socket
+import socket,pyttsx3
 from cryptography.fernet import Fernet
 target_ip = "127.0.0.1"
-target_port = 1235
+target_port = 1236
 # now we are creating udp socket -- this is for all sender and reciever
 # it means we use ipv4,UDP
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -14,16 +14,30 @@ while True:
 	cipher_key=b'GzDqzcu4kz52ZOyu7haSaWY4t4mE2jzSKD4JuYfm7VE='
 	#cipher_key = Fernet.generate_key()
 	#print(cipher_key)
-	cipher = Fernet(cipher_key)
-	message = msg.encode()
-	encrypted = cipher.encrypt(message)
+	cipher = Fernet(cipher_key) # initailizing
+	message = msg.encode() # to convert it to bytes
+	encrypted = cipher.encrypt(message) #CONVERTING TO ENCRYTED MESSAGE	
 	#new_msg = msg.encode('ascii')
 	#message = b'the quick brown fox jumps over the lazy dog'
 	#encrypted_message = cipher.encrypt(message)
 	out_message=encrypted.decode()# turn it into a string to send
-	new_msg = out_message.encode('ascii')
+	new_msg = out_message.encode('ascii') # it will sent in bytes in ascii
 	#print(new_msg)
 	# we have to encode string to byte like object in python3 only
-	s.sendto(new_msg,(target_ip,target_port))
-	print(s.recvfrom(100))
+	s.sendto(new_msg,(target_ip,target_port)) # sending the message
+	# reply section
+	client_data = s.recvfrom(1000) # recieving the reply
+	#client_data_s = client_data[0].decode('ascii') # converting the message to string
+	#print(client_data_s)
+	#client_data_b = client_data_s.encode() # converting the message to bytes
+	print(client_data[0])
+	cipher = Fernet(cipher_key) # initailizing
+	decrypted = cipher.decrypt(client_data[0]) # decryting the message
+	decrypted_message = decrypted.decode('ascii')
+	print(decrypted_message)
+	audio2 = pyttsx3.init()
+	audio2.say(decrypted_message)
+	audio2.runAndWait()
+	print("the decrypted message is ",decrypted_message) # printing the decrypted message
+	
  
